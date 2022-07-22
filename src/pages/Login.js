64e1fdc183e912c8c0ec162bb7742067cdb82d9e
Gabriel Garcia <philiasmaximus@gmail.com>
2022-07-22 19:35:41 -0300

    Requisito 1 feito Lint corrigido

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,28 +1,42 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
-import { createUser } from '../services/userAPI';
+import PropTypes from 'prop-types';
+// import { Redirect } from 'react-router-dom';
+// import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
   render() {
-    const { loginButtonDisabled, loginName, onInputChange, onLoginClick } = this.props
+    const { loginButtonDisabled, loginName, onInputChange, onLoginClick } = this.props;
     return (
       <div data-testid="page-login">
         <form>
-          <input 
-            name="loginName" 
-            type="text" 
-            data-testid="login-name-input" 
+          <input
+            name="loginName"
+            type="text"
+            data-testid="login-name-input"
             value={ loginName }
-            onChange={ onInputChange }></input>
-          <button 
-            name="loginButton " 
-            type="button" data-testid="login-submit-button" 
+            onChange={ onInputChange }
+          />
+          <button
+            name="loginButton "
+            type="button"
+            data-testid="login-submit-button"
             disabled={ loginButtonDisabled }
-            onClick={ onLoginClick }>Entrar</button>
+            onClick={ onLoginClick }
+          >
+            Entrar
+
+          </button>
         </form>
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  loginName: PropTypes.string.isRequired,
+  loginButtonDisabled: PropTypes.bool.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  onLoginClick: PropTypes.func.isRequired,
+};
 
 export default Login;
